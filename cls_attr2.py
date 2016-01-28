da7ce04c17465fb8cd633ba7c640dd8d2360e690
Yu Zhang <yzhang@purestorage.com>
2016-01-28 12:59:14 -0800

    Demos how class stores variables for each instance
    - And what is done for attribute access with the dot operator

---
+++ cls_attr2.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+"""To peak into attributes of class and instances more closely.
+This would explain why dog_a.name would give 'lina', and dog_b.name would give
+'losh'.
+Class and objects supports access to attributes both through the attribute operator,
+(Implemented via the class or its metaclass's __getattribute__ method). To be more
+specific:
+    1. Accessing instance attributes as in a.b invokes python to call:
+    type(a).__getattrbute__(a, 'b')
+
+    2. Assigning values to instance attributes as in a.b = 'something' invokes python
+    to call:
+    type(a).__setattr__(a, 'b', 'something'):
+
+With that implemented explicitely by overriding the metaclass's __getattribute__
+and __setattr__ functions, the following logic simply follows.
+
+assgning dog_a.name = 'lina' would invoke:
+    -> Dog.__setattr__(dog_a, 'name', 'lina')
+    which invokes:
+    -> Dog.__dict__['__dict__'].__get__(dog_a, Dog)['name'] = 'lina'
+
+accessing dog_a.name would invoke:
+    -> Dog.__getattribute__(dog_a, 'name')
+    which invokes:
+    -> Dog.__dict__['__dict__'].__get__(dog_a, Dob)['name']
+
+That's how a class stores variables for different instances.
+There could be more complecation when it comes to how class
+store their attributes. But it wouldn't be so hard with the
+idea that class is also objects in python.
+"""
+import pdb
+
+class MyGetSetDescriptor(object):
+    def __init__(self, name):
+        self.name = name
+        self.all_insts_vars = {}
+
+    def __get__(self, instance, cls):
+        if instance not in self.all_insts_vars:
+            self.all_insts_vars[instance] = dict()
+            instance.__dict__  = self.all_insts_vars[instance]
+        return self.all_insts_vars[instance]
+
+
+class Dog(object):
+    __dict__ = MyGetSetDescriptor(name='__dict__')
+    color = 'red'
+
+    def __init__(self, name):
+        self.name = name
+
+    def __getattribute__(self, attr_name):
+        print '\nVerbose getter\n'
+        try:
+            return Dog.__dict__['__dict__'].__get__(self, Dog)[attr_name]
+        except KeyError:
+            pass
+
+        try:
+            return Dog.__dict__[attr_name].__get__(self, Dog)
+        except AttributeError:
+            pass
+
+        try:
+            return Dog.__dict__[attr_name]
+        except KeyError:
+            raise
+
+    def __setattr__(self, attr_name, value):
+        print '\nVerbose setter\n'
+        Dog.__dict__['__dict__'].__get__(self, Dog)[attr_name] = value
+
+    def __delattr__(self, sttr_name):
+        print '\nVerbose Deletter\n'
+        del Dog.__dict__['__dict__'].__get__(self, Dog)[attr_name]
+
+
+
+if __name__ == '__main__':
+    dog_a = Dog('lina')
+    dog_b = Dog('losh')
+    print dog_a.name, dog_b.name
+    dog_a.weight = 3
+    dog_b.weight = 5
+    print dog_a.weight, dog_b.weight
+    print dog_a.__dict__, dog_b.__dict__
+    print type(dog_a.__dict__)
+    print type(Dog.__dict__['__dict__'])
+    print dog_a.color, dog_b.color
+    del dog_a.weight
+    print dog_a.weight
